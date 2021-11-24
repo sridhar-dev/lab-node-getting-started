bb31c57a7de563a6b34c5a13345fd0479ccc8905
sridhar-dev <sri85213@gmail.com>
2021-11-24 15:41:50 +0530

    4675

--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 class SortedList {
+
   constructor() {
-  
+
+        this.items = [],
+        this.length = 0
+
   }
-  add() {
+
+  add(item) {
+    let old_items = this.items;
+    old_items.push(item);
+    let sorting = old_items.sort((a, b) => a - b);
+    
+      this.items =  sorting;
+      this.length = this.items.length;
+    
+
    
   }
-  get() {
+  get(pos) {
+
+    if(pos > this.length - 1) {
+      throw new Error('OutOfBounds');
+    }
+
+    else{
+      return this.items[pos];
+    }
 
   }
   max() {
 
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    else{
+     return Math.max(...this.items);
+    }
+
   }
   min() {
 
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    else{
+     return Math.min(...this.items);
+    }
+
+
   }
   avg() {
+
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    else{
+      return this.sum()/this.length;
+      
+    }
     
    
   }
 
   sum() {
+
+   
+      let sum = this.items.reduce((a,b) => {
+        return b+a;
+      },0)
+
+      return sum;
+    }
   
-  }
+  
   
 };
 
+const object=new SortedList();
 module.exports = SortedList;
